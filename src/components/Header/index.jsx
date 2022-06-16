0a051f91b53804ecef10fbb7b33a0b656df86b51
Zavierrr <1136415245@qq.com>
2022-06-16 16:53:33 +0800

    第一版本使用useLocation hooks 完成pathname获取

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import { pageTitle } from '../../config'
 
 export default function Header(props) {
+    // const { title = "饿了么" } = props
+    // let title = ''
+    // 赋初值
+    const { pathname = '/' } = useLocation()
     const [title, setTitle] = useState('首页')
-
     useEffect(() => {
-        let _title;
-        const pathname = window.location.pathname;
-        if (pathname == '/order') {
-            _title = '订单'
-        } else if (pathname == '/home' || pathname == '/') {
-            _title = '首页'
-        }
+        // const location = window.location.pathname;
+        let _title = pageTitle[pathname] || '';
+        // if (pathname == '/order') {
+        //     _title = '订单'
+        // } else if (pathname == '/home' || pathname == '/') {
+        //     _title = '首页'
+        // }
         setTitle(_title)
     })
     return (
