bf230ac5b290555fb5fdf9e9ca364e62de9afc98
Zavierrr <1136415245@qq.com>
2022-06-16 16:52:41 +0800

    第一版本

--- src/pages/Order/index.jsx
+++ src/pages/Order/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Home() {
+export default function Order() {
     return (
         <div>
-            Home
+            Order
         </div>
     )
 }
