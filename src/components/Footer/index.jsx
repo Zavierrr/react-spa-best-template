319ff356a1cd97cf6a84b161cadcdfbdf667f50f
Zavierrr <1136415245@qq.com>
2022-06-16 19:48:19 +0800

    用font-awesome引入图标

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import { FooterWrapper } from './style'
 
 export default function Footer(props) {
+    const { pathname } = useLocation()
     return (
-        <footer></footer>
+        <FooterWrapper>
+            <Link to="/home">
+                <i className="fa fa-home"></i>
+                <span>首页</span>
+            </Link>
+            <Link to="/find">
+                <i className="fa fa-postcast"></i>
+                <span>发现</span>
+            </Link>
+            <Link to="/order">
+                <i className="fa fa-book"></i>
+                <span>订单</span>
+            </Link>
+            <Link to="/find">
+                <i className="fa fa-user"></i>
+                <span>我的</span>
+            </Link>
+        </FooterWrapper>
     )
 }
---
+++ src/readme.md
@@ -0,0 +1,18 @@
+- react 项目初始化之优化
+    1. git 版本控制系统也是工程化的一部分
+    2. 开源思维 找 + 供
+    3. 加速
+
+- 页面layout
+    App 组件
+    1. 页面级别组件共享布局方案
+        header + 动态路由租价（Routes + Route path） + footer
+
+- header 组件的版本化及配置
+    1. 页面中layout 固定部分，不属于页面组件的内部，属于layout
+    2. BOM location.pathname 第一版本
+
+- 图标字体库
+    1. font-awesome
+        方便，缺点：没有制定性
+    2. iconfont 网站找，下载代码包
