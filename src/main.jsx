ed45d532ffdf274a5486c4b076c41485f1c1dacc
Zavierrr <1136415245@qq.com>
2022-06-21 16:37:43 +0800

    更新了antd框架

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,8 @@ import './assets/styles/reset.css'
 import './index.css'
 import './modules/rem' // 自适应
 import 'swiper/dist/css/swiper.min.css'
-import 'antd-mobile/dist/antd-mobile.css'
+// 最新版本无需引入css
+// import 'antd-mobile/dist/antd-mobile.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
